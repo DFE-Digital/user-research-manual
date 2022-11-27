2483174832d6c3a07b5c8fa902f06dbcd9dba079
tomadams <tomadams@SBWT620556.AD.HQ.DEPT>
2022-11-27 19:09:11 +0000

    Initial content added

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -19,3 +19,30 @@ Array.prototype.forEach.call(cards, function (card) {
         }
     };
 });
+
+(function ($, Modules) {
+    'use strict'
+    Modules.AnchoredHeadings = function () {
+      this.start = function ($element) {
+        var headings = $element.find('h1, h2, h3, h4, h5, h6')
+        headings.each(injectAnchor)
+      }
+  
+      function injectAnchor () {
+        var $this = $(this)
+    
+        if ($this.attr('id')) {        
+          $this.addClass('anchored-heading')
+          $this.prepend(
+            '<a href="#' + $this.attr('id') + '" class="anchored-heading__icon" aria-hidden="true" tabindex="-1"></a>'
+          )
+        }
+      }
+    }
+  })(jQuery, window.GOVUK.Modules);
+
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, 'clarity', 'script', 'driuzqz382');
