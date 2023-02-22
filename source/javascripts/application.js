7a7e8378b392ffdad5e7a118f8bfad3cf6765228
Andy Jones <42679439+andyjones81@users.noreply.github.com>
2023-02-22 10:46:45 +0000

    Updated the JS to link to the correct clarity code

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -41,8 +41,9 @@ Array.prototype.forEach.call(cards, function (card) {
     }
   })(jQuery, window.GOVUK.Modules);
 
+
 (function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, 'clarity', 'script', 'driuzqz382');
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "fy652qyfaj");
